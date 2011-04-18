974fa1d6675edf90456b92164ebc5d7b9f0c9afe
Bodil Stokke <bodil@bodil.tv>
2011-04-18 20:29:25 +0200

    Give Todochiku some lovely Ubuntu icons.

--- custom.el
+++ custom.el
@@ -10,6 +10,8 @@
  '(inferior-lisp-program "cake repl")
  '(tabbar-mode t nil (tabbar))
  '(tabbar-mwheel-mode t nil (tabbar))
+ '(todochiku-icons-directory "~/.emacs.d/todochiku-icons")
+ '(todochiku-icons (quote ((default . "default.svg") (alert . "alert.svg") (bell . "bell.svg") (compile . "compile.svg") (irc . "chat.svg") (check . "check.svg") (emacs . "emacs.svg") (star . "star.svg") (social . "social.svg") (alarm . "alarm.svg") (music . "music.svg") (mail . "mail.svg") (term . "terminal.svg") (package . "package.svg"))))
  '(whole-line-or-region-extensions-alist (quote ((copy-region-as-kill whole-line-or-region-copy-region-as-kill nil) (kill-region whole-line-or-region-kill-region nil) (kill-ring-save whole-line-or-region-kill-ring-save nil) (yank whole-line-or-region-yank nil) (comment-dwim whole-line-or-region-comment-dwim-2 nil)))))
 
 (custom-set-faces
