619655ef39889a3c26a06465e22d941ae1750df2
Bodil Stokke <bodil@bodil.tv>
2011-04-20 15:01:09 +0200

    Some lines to make paredit play nicer.

--- bodil-lisp.el
+++ bodil-lisp.el
@@ -1,3 +1,16 @@
 ;;; bodil-lisp.el -- Lisp specific configuration
 
+;; Make paredit play nice with eldoc
+(require 'eldoc)
+(eldoc-add-command
+ 'paredit-backward-delete
+ 'paredit-close-round)
+
+;; And make paredit play nice with SLIME:
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
 
