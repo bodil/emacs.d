fc77a0f3475ccb06a57d1adb2b02b3345f558e9d
Bodil Stokke <bodil@bodil.tv>
2011-04-21 20:32:53 +0200

    Make ERC use Todochiku notifications on privmsg.

--- bodil-erc.el
+++ bodil-erc.el
@@ -11,10 +11,18 @@
     (let ((network (car elt)) (password (cadr elt)))
       (erc :server "bodil.wtf.la" :port 1337 :nick network :password (concat network ":" password)))))
 
+;; Prioritise privmsgs maximally
 (defadvice erc-track-find-face (around erc-track-find-face-promote-query activate)
   (if (erc-query-buffer-p) 
       (setq ad-return-value (intern "erc-current-nick-face"))
     ad-do-it))
+
+;; Desktop notifications on privmsg
+(defun erc-desktop-notification-on-privmsg (proc msg)
+  (interactive)
+  (when (erc-current-nick-p (car (erc-response.command-args msg)))
+    (todochiku-message (erc-response.sender msg) (erc-response.contents msg) (todochiku-icon 'irc))))
+(add-hook 'erc-server-PRIVMSG-functions 'erc-desktop-notification-on-privmsg)
 
 (setq erc-modules (quote (button completion dcc fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom stamp track)))
 (setq erc-track-exclude-server-buffer t)
@@ -23,3 +31,6 @@
 (setq erc-track-showcount nil)
 (setq erc-track-switch-direction (quote importance))
 (setq erc-header-line-uses-tabbar-p t)
+
+
+
