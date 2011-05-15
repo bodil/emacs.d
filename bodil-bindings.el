c77aa5cfe65cd1917bad06ab23b12b0cd303e0bf
Bodil Stokke <bodil@bodil.tv>
2011-05-15 13:49:10 +0200

    Combine ido and imenu to jump to a symbol in file.

--- bodil-bindings.el
+++ bodil-bindings.el
@@ -96,3 +96,36 @@
 ;; Bind Flyspell completion key to M-\
 (setq flyspell-auto-correct-binding (kbd "M-\\"))
 
+;; Setup a function to jump to a symbol in the current file
+(defun ido-goto-symbol ()
+  "Will update the imenu index and then use ido to select a 
+   symbol to navigate to"
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols (symbol-list)
+                       (when (listp symbol-list)
+                         (dolist (symbol symbol-list)
+                           (let ((name nil) (position nil))
+                             (cond
+                              ((and (listp symbol) (imenu--subalist-p symbol))
+                               (addsymbols symbol))
+   
+                              ((listp symbol)
+                               (setq name (car symbol))
+                               (setq position (cdr symbol)))
+   
+                              ((stringp symbol)
+                               (setq name symbol)
+                               (setq position (get-text-property 1 'org-imenu-marker symbol))))
+   
+                             (unless (or (null position) (null name))
+                               (add-to-list 'symbol-names name)
+                               (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (goto-char position))))
+(global-set-key (kbd "C-t") 'ido-goto-symbol)
+
