360c8ac329cd1940e4652e64f6dbe6f888a30eb8
Bodil Stokke <bodil@bodil.tv>
2011-06-05 20:07:16 +0200

    I may live to regret this: enable "smart" home key behaviour, even for C-a.

--- bodil-bindings.el
+++ bodil-bindings.el
@@ -170,3 +170,18 @@
     (exchange-point-and-mark)))
 (global-set-key (kbd "C-M-<down>") 'duplicate-start-of-line-or-region)
 
+;; Smart home key
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive "^")
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+(global-set-key (kbd "<home>") 'smart-beginning-of-line)
+(global-set-key (kbd "C-a") 'smart-beginning-of-line)
+
+
