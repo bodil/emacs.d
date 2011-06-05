90e2602c9cd4dd199c2edeacb31bba6d7078adfc
Bodil Stokke <bodil@bodil.tv>
2011-06-05 19:53:20 +0200

    Add line or region duplication from http://www.emacswiki.org/emacs/DuplicateStartOfLineOrRegion and make it copy the whole line if cursor is at beginning-of-line.

--- bodil-bindings.el
+++ bodil-bindings.el
@@ -138,3 +138,32 @@
 ;; Keep autopair from interfering with auto-complete
 (setq ac-use-overriding-local-map t)
 
+;; Duplicate start of line or region, from http://www.emacswiki.org/emacs/DuplicateStartOfLineOrRegion
+(defun duplicate-start-of-line-or-region ()
+  (interactive)
+  (if mark-active
+      (duplicate-region)
+    (duplicate-start-of-line)))
+(defun duplicate-start-of-line ()
+  (if (bolp)
+      (progn
+        (end-of-line)
+        (duplicate-start-of-line)
+        (beginning-of-line))
+    (let ((text (buffer-substring (point)
+                                  (beginning-of-thing 'line))))
+      (forward-line)
+      (push-mark)
+      (insert text)
+      (open-line 1))))
+(defun duplicate-region ()
+  (let* ((end (region-end))
+         (text (buffer-substring (region-beginning)
+                                 end)))
+    (goto-char end)
+    (insert text)
+    (push-mark end)
+    (setq deactivate-mark nil)
+    (exchange-point-and-mark)))
+(global-set-key (kbd "C-M-<down>") 'duplicate-start-of-line-or-region)
+
