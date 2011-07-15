1a7752d77435bdc30627c7cd7ef0143826d86d27
Bodil Stokke <bodil@bodil.tv>
2011-07-16 00:08:13 +0200

    Wants auto-complete in coffee-mode.

--- bodil-js.el
+++ bodil-js.el
@@ -28,6 +28,7 @@
   (define-key coffee-mode-map (kbd "M-R") 'coffee-compile-region))
 (add-hook 'coffee-mode-hook
           '(lambda() (coffee-custom)))
+(add-to-list 'ac-modes 'coffee-mode)
 
 ;; JSLint for Flymake
 ;; (require 'flymake-lintnode)
