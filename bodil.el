31e6c2809223c1fbf1cf787d950be89e8977ca88
Bodil Stokke <bodil@bodil.tv>
2011-07-19 19:37:34 +0200

    Just add all subdirs of site-lisp to the load path. Adding each one manually is becoming really stupid.

--- bodil.el
+++ bodil.el
@@ -26,28 +26,18 @@
 (defmacro Xlaunch (&rest x)
   (list 'if (eq window-system 'x) (cons 'progn x)))
 
-;; Configure load path
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/slime"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/swank-js"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/clojure-mode"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/haskell-mode"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/malabar/lisp"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/ecb"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/auto-complete"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/yasnippet"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/zenburn"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/wl"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/smart-tab"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/undohist"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/multi-term"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/coffee-mode"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/rfringe"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/lintnode"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/emacs-w3m"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/magit"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/lolcode-mode"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/twittering-mode"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/jade-mode"))
+;; Add every subdirectory of ~/.emacs.d/site-lisp to the load path
+(dolist (elt (delq nil
+                   (mapcar
+                    (lambda (filedesc)
+                      (let ((filename (car filedesc))
+                            (isdirectory (cadr filedesc)))
+                        (if (and isdirectory
+                                 (not (integerp (string-match "/\\.+$" filename))))
+                            filename
+                          nil)))
+                    (directory-files-and-attributes "~/.emacs.d/site-lisp" t))))
+  (add-to-list 'load-path elt))
 
 ;; Load more scripts
 (load "~/.emacs.d/bodil-theme")
