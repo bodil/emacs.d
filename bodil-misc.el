0b7a9a21ab0497c113e1198586fbfb4de5a930df
Bodil Stokke <bodil@bodil.tv>
2011-07-30 12:01:25 +0200

    Added evernote-mode.

--- bodil-misc.el
+++ bodil-misc.el
@@ -76,3 +76,17 @@
       '(":home"
         "bodiltv/steria-devs"
         ":replies"))
+
+;; evernote-mode
+(require 'evernote-mode)
+(setq evernote-username "bodil")
+(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; option
+(global-unset-key (kbd "C-c e"))
+(global-set-key "\C-cec" 'evernote-create-note)
+(global-set-key "\C-ceo" 'evernote-open-note)
+(global-set-key "\C-ces" 'evernote-search-notes)
+(global-set-key "\C-ceS" 'evernote-do-saved-search)
+(global-set-key "\C-cew" 'evernote-write-note)
+(global-set-key "\C-cep" 'evernote-post-region)
+(global-set-key "\C-ceb" 'evernote-browser)
+
