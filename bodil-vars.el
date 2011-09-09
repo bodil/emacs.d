47c831dc5c55c44ecf09d39ba73c715f60e4e088
Bodil Stokke <bodil@bodil.tv>
2011-09-09 20:44:11 +0200

    Save/restore the desktop.

--- bodil-vars.el
+++ bodil-vars.el
@@ -38,3 +38,8 @@
 (setq iflipb-ignore-buffers '("*Messages*" "*Compile-Log*" "*Malabar Compilation*" "*scratch*"
                               "*lintnode*"))
 
+;; Configure desktop saving
+(setq desktop-restore-eager 5)
+(setq desktop-save (quote if-exists))
+(desktop-save-mode 1)
+
