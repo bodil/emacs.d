36af9d4b3053dcd6612e9e4d29fffc333b12f4d4
Bodil Stokke <bodil@bodil.tv>
2011-09-16 12:02:47 +0200

    Make region and hl-line bg more visible.

--- bodil-theme.el
+++ bodil-theme.el
@@ -10,7 +10,8 @@
     (color-theme-zenburn)
     (set-face-underline-p 'highlight nil)
     ))
-(set-face-background 'highlight "#3a3a3a")
+(set-face-background 'highlight "#363636")
+(set-face-background 'region "#464668")
 
 ;; Highlight current line
 (setq global-hl-line-mode t)
