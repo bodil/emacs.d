997878ec11cec01c46ff1090ed9258a633e1f85d
Bodil Stokke <bodil@bodil.tv>
2012-01-07 00:28:56 +0100

    Remove Flymake JSLint.

--- bodil-js.el
+++ bodil-js.el
@@ -28,23 +28,6 @@
 (add-to-list 'ac-modes 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.cson$" . coffee-mode))
 
-;; JSLint for Flymake
-(require 'flymake)
-(defun flymake-jslint-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "jslint" (list local-file))))
-(setq flymake-err-line-patterns 
-      (cons '("^  [[:digit:]]+ \\([[:digit:]]+\\),\\([[:digit:]]+\\): \\(.+\\)$"  
-              nil 1 2 3)
-            flymake-err-line-patterns))
-(add-to-list 'flymake-allowed-file-name-masks
-             '("\\.js\\'" flymake-jslint-init))
-(add-hook 'js2-mode-hook (lambda () (flymake-mode t)))
-
 ;; Patch coffee-mode so coffee-compile-region pops up a new
 ;; non-focused window instead of replacing the current buffer.
 (defun coffee-compile-region (start end)
