358f5c79467d778224725dcc61f3d9a34e5b7f8f
Bodil Stokke <bodil@bodil.tv>
2012-01-27 22:53:16 +0100

    emacs.d needs more @magnars.

--- bodil-misc.el
+++ bodil-misc.el
@@ -105,3 +105,18 @@
 
 ;; writegood-mode
 (require 'writegood-mode)
+
+;; mark-multiple
+(require 'inline-string-rectangle)
+(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+
+(require 'mark-more-like-this)
+(global-set-key (kbd "C-<") 'mark-previous-like-this)
+(global-set-key (kbd "C->") 'mark-next-like-this)
+(global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
+
+;; (require 'rename-sgml-tag)
+;; (define-key sgml-mode-map (kbd "C-c C-r") 'rename-sgml-tag)
+
+;; (require 'js2-rename-var)
+;; (define-key js2-mode-map (kbd "C-c C-r") 'js2-rename-var)
