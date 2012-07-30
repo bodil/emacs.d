8704e36e52273c340115fc1254ce19ae3ea02094
Bodil Stokke <bodil@bodil.tv>
2012-07-30 15:08:17 +0200

    Tune Markdown output.

--- bodil-markdown.el
+++ bodil-markdown.el
@@ -3,7 +3,7 @@
 (when (not (package-installed-p 'markdown-mode))
   (package-install 'markdown-mode))
 
-(setq-default markdown-command "pandoc -f markdown -t html5 ")
+(setq-default markdown-command "pandoc -S -s --self-contained -f markdown -t html5 ")
 
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
