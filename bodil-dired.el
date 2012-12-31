dcf90f7bc8ecb2db6543dc7976126971d8db5d9c
Bodil Stokke <bodil@bodil.tv>
2012-12-31 14:10:20 +0100

    Auto refresh dired buffers.

--- bodil-dired.el
+++ bodil-dired.el
@@ -1,3 +1,10 @@
 ;;; bodil-dired.el -- Dired extensions
 
 (require 'dired+)
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
