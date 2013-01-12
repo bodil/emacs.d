94d989134c15a6481ae4ad1d7fbae5abb9e2ef41
Bodil Stokke <bodil@bodil.tv>
2013-01-12 16:11:18 +0100

    Provide facility for automatically installing package dependencies.

--- init.el
+++ init.el
@@ -33,6 +33,18 @@
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq custom-file (concat dotfiles-dir "custom.el"))
 
+;; Detect online status, from ESK
+
+(require 'cl)
+(defun esk-online? ()
+  (if (and (functionp 'network-interface-list)
+           (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+                         (member 'up (first (last (network-interface-info
+                                                   (car iface)))))))
+            (network-interface-list))
+    t))
+
 ;; ELPA
 (setq package-user-dir (concat dotfiles-dir "elpa"))
 (require 'package)
@@ -41,6 +53,13 @@
                   ("elpa" . "http://tromey.com/elpa/")))
   (add-to-list 'package-archives source t))
 (package-initialize)
+(when (esk-online?)
+  (unless package-archive-contents (package-refresh-contents)))
+
+(defun package-require (pkg)
+  "Install a package only if it's not already installed."
+  (when (not (package-installed-p pkg))
+    (package-install pkg)))
 
 ;; Write backup files to own directory
 (setq backup-directory-alist
