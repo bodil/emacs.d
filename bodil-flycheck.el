2c74800ae298a275c9bd8e79ee8186f4d238773a
Bodil Stokke <bodil@bodil.tv>
2013-01-27 22:35:24 +0100

    Skip flycheck in emacs-lisp-mode.

--- bodil-flycheck.el
+++ bodil-flycheck.el
@@ -1,7 +1,10 @@
 ;;; bodil-flycheck.el --- Flycheck setup
 
 (package-require 'flycheck)
-(add-hook 'find-file-hook 'flycheck-mode)
+(add-hook 'find-file-hook
+          (lambda ()
+            (when (not (equal 'emacs-lisp-mode major-mode))
+              (flycheck-mode))))
 
 (global-set-key (kbd "M-n") 'next-error)
 (global-set-key (kbd "M-p") 'previous-error)
