f30b9a92935f2aa52357cb05141b83fca6378224
Bodil Stokke <bodil@bodil.tv>
2013-01-27 22:21:46 +0100

    Simpler keybindings for next-error and previous-error.

--- bodil-flycheck.el
+++ bodil-flycheck.el
@@ -3,4 +3,7 @@
 (package-require 'flycheck)
 (add-hook 'find-file-hook 'flycheck-mode)
 
+(global-set-key (kbd "M-n") 'next-error)
+(global-set-key (kbd "M-p") 'previous-error)
+
 (provide 'bodil-flycheck)
