aff90584374cc18c951eb8fcf96ca34c3f4d9938
Bodil Stokke <bodil@bodil.tv>
2013-04-20 20:12:46 +0200

    Restrict flycheck annotations to the fringe.

--- bodil-flycheck.el
+++ bodil-flycheck.el
@@ -9,4 +9,12 @@
 (global-set-key (kbd "M-n") 'next-error)
 (global-set-key (kbd "M-p") 'previous-error)
 
+(eval-after-load "flycheck"
+  '(progn
+     (setq flycheck-highlighting-mode nil)
+     (set-face-background 'flycheck-error-face "red")
+     (set-face-foreground 'flycheck-error-face "black")
+     (set-face-background 'flycheck-warning-face "orange")
+     (set-face-foreground 'flycheck-warning-face "black")))
+
 (provide 'bodil-flycheck)
