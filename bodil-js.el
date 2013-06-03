1d5a8afaf9526ed1483feb733c340b262f17195a
Bodil Stokke <bodil@bodil.tv>
2013-06-03 11:13:59 +0200

    Completely disregard js2-mode strict warnings; rely entirely on flycheck.

--- bodil-js.el
+++ bodil-js.el
@@ -16,8 +16,12 @@
 (setq-default js2-indent-on-enter-key t)
 (setq-default js2-mode-indent-ignore-first-tab t)
 (setq-default js2-show-parse-errors nil)
+(setq-default js2-strict-inconsistent-return-warning nil)
+(setq-default js2-strict-var-hides-function-arg-warning nil)
 (setq-default js2-strict-missing-semi-warning nil)
 (setq-default js2-strict-trailing-comma-warning nil)
+(setq-default js2-strict-cond-assign-warning nil)
+(setq-default js2-strict-var-redeclaration-warning nil)
 (setq-default js2-global-externs
       '("module" "require" "__dirname" "process" "console" "define"
         "JSON" "$" "_" "Backbone" "buster" "sinon" "moment" "_gaq"
