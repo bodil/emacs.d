da7517e03f3985ab2e0e19e734fb057366d99c6e
Bodil Stokke <bodil@bodil.org>
2013-07-26 21:15:33 +0200

    Add Varnish config file mode.

--- bodil-misc-modes.el
+++ bodil-misc-modes.el
@@ -1,6 +1,11 @@
 ;;; bodil-misc-modes.el -- Miscellaneous modes.
 
+;; Puppet config files
 (package-require 'puppet-mode)
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
+
+;; Varnish config files
+(package-require 'vcl-mode)
+(add-to-list 'auto-mode-alist '("\\.vcl$" . vcl-mode))
 
 (provide 'bodil-misc-modes)
