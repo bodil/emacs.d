724382927146541b0420601855ad03a1d51afe61
Bodil Stokke <bodil@bodil.org>
2013-08-09 21:53:40 +0200

    magit-inotify package appears to have vanished from ELPA.

--- bodil-magit.el
+++ bodil-magit.el
@@ -4,9 +4,6 @@
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; Automatically refresh changes in status buffer
-(package-require 'magit-inotify)
-
 ;; Github integration
 (package-require 'gist)
 
