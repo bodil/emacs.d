173abb25ff689c6359b489237c4a38b5b20d9f10
Bodil Stokke <bodil@bodil.org>
2013-11-19 16:07:17 +0000

    Enable helm-backup.

--- bodil-helm.el
+++ bodil-helm.el
@@ -7,4 +7,10 @@
 (package-require 'helm-projectile)
 (global-set-key (kbd "C-c <print>") 'helm-projectile)
 
+;; Backup
+(package-require 'helm-backup)
+(add-hook 'after-save-hook 'helm-backup-versioning)
+(global-set-key (kbd "C-c b") 'helm-backup)
+(setq helm-backup-path "~/.emacs.d/.helm-backup")
+
 (provide 'bodil-helm)
