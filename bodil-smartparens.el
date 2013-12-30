7badbdcf3dfc9a27960f45bedeed8f256d9bdf79
Bodil Stokke <bodil@bodil.org>
2013-12-30 14:27:52 +0000

    Smartparens tweaks:
    * Auto-insert newline between curly braces in a new C style block, and
      position the cursor appropriately, like autopair used to do.
    * Insert a space after new sexp immediately preceding a word in Lisp mode,
      like paredit used to do.

--- bodil-smartparens.el
+++ bodil-smartparens.el
@@ -29,4 +29,34 @@
   (sp-local-pair "{" nil :bind "M-{")
   (sp-local-pair "\"" nil :bind "M-\""))
 
+;; Add newline and position cursor appropriately when starting a
+;; curly brace block in C like modes
+
+(defun sp--my-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(sp-with-modes '(c-mode c++-mode js-mode js2-mode java-mode
+                        typescript-mode perl-mode)
+  (sp-local-pair "{" nil :post-handlers
+                 '((sp--my-create-newline-and-enter-sexp "RET"))))
+
+;; Insert space after pair if immediately followed by a word in Lisp modes
+
+(defun sp--my-add-space-after-sexp-insertion (id action _context)
+  (when (eq action 'insert)
+    (save-excursion
+      (forward-char (length (plist-get (sp-get-pair id) :close)))
+      (when (or (eq (char-syntax (following-char)) ?w)
+                (looking-at (sp--get-opening-regexp)))
+        (insert " ")))))
+
+(sp-with-modes bodil-lisp-modes
+  (sp-local-pair "(" nil :post-handlers
+                 '(:add sp--my-add-space-after-sexp-insertion)))
+
+
 (provide 'bodil-smartparens)
