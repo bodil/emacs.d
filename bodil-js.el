05c488985467fe81f43550aea8660d53bbcbe06b
Bodil Stokke <bodil@bodil.org>
2014-02-17 20:20:57 +0000

    Flychecker for tslint.

--- bodil-js.el
+++ bodil-js.el
@@ -120,4 +120,16 @@
 (package-require 'tss)
 (add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
 
+(package-require 'flycheck)
+(require 'flycheck)
+(flycheck-define-checker tslint
+  "Use tslint to flycheck TypeScript code."
+  :command ("tslint"
+            "-f" source
+            "-c" (eval (projectile-expand-root "tslint.json"))
+            "-t" "prose")
+  :error-patterns ((warning (file-name) "[" line ", " column "]: " (message)))
+  :modes typescript-mode)
+(add-to-list 'flycheck-checkers 'tslint)
+
 (provide 'bodil-js)
