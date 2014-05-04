1b478d879c4d7717339faaccfb28fe5ca3fc770b
Bodil Stokke <bodil@bodil.org>
2014-05-04 22:33:04 +0100

    Treat the ".es6" extension as JS.

--- bodil-js.el
+++ bodil-js.el
@@ -5,7 +5,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;; Edit ejs as js
+;; Alternative js2-mode extensions
+(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . js2-mode))
 
 (font-lock-add-keywords
