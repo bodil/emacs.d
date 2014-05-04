6989407aa67e88a786a160f50759fa41faaf9550
Bodil Stokke <bodil@bodil.org>
2014-05-04 22:30:23 +0100

    Attempt to turn on js-mode on Pink slides containing a JS editor.

--- bodil-multimodes.el
+++ bodil-multimodes.el
@@ -11,6 +11,20 @@
         ;; CSS inside HTML
         (html-mode nil html-css)))
 
+(mmm-add-group
+ 'html-js-deck
+ '((js-section
+    :submode js-mode
+    :face mmm-code-submode-face
+    :front "<section[^>]*data-editor=\"text/javascript\"[^>]*>[ \t]*\n?"
+    :back "[ \t]*</section>"
+    :insert ((?j js-tag nil @ "<section data-editor=\"text/javascript\">\n"
+                 @ "" _ "" @ "\n</section>" @)))))
+
+(mmm-add-mode-ext-class 'html-mode nil 'html-js-deck)
+
+;; <section data-editor="text/javascript"
+
 ;; Prefer js2-mode for inline JS
 (mmm-add-to-major-mode-preferences 'javascript 'js2-mode t)
 
