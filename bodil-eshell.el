f0e02bcd7581c5e17e3f58c9e5cc6f82ddcc1b42
Bodil Stokke <bodil@bodil.org>
2014-05-17 22:29:09 +0100

    Fancier eshell prompt.

--- bodil-eshell.el
+++ bodil-eshell.el
@@ -4,23 +4,24 @@
 (require 'f)
 
 (setq eshell-visual-commands
-      '("less" "tmux" "htop" "top"))
+      '("less" "tmux" "htop" "top" "bash" "zsh" "fish"))
 
 (setq eshell-visual-subcommands
       '(("git" "log" "l" "diff" "show")))
 
-;; Prompt from http://www.emacswiki.org/emacs/EshellPrompt
+;; Prompt with a bit of help from http://www.emacswiki.org/emacs/EshellPrompt
 (defmacro with-face (str &rest properties)
   `(propertize ,str 'face (list ,@properties)))
 
 (defun eshell/abbr-pwd ()
   (let ((home (getenv "HOME"))
         (path (eshell/pwd)))
-    (if (f-ancestor-of? home path)
-        (concat "~/" (f-relative path home))
-      path)))
+    (cond
+     ((string-equal home path) "~")
+     ((f-ancestor-of? home path) (concat "~/" (f-relative path home)))
+     (path))))
 
-(defun shk-eshell-prompt ()
+(defun eshell/my-prompt ()
   (let ((header-bg "#161616"))
     (concat
      (with-face user-login-name :foreground "#d75faf")
@@ -32,7 +33,7 @@
        (with-face "$" :foreground "#2345ba"))
      " ")))
 
-(setq eshell-prompt-function 'shk-eshell-prompt)
+(setq eshell-prompt-function 'eshell/my-prompt)
 (setq eshell-highlight-prompt nil)
 (setq eshell-prompt-regexp "^[^#$\n]+[#$] ")
 
