de29583e71d37cc3728ab4941a386120656c4d0e
Bodil Stokke <bodil@bodil.org>
2014-06-07 20:36:59 +0100

    Disable require-final-newline to keep ethan-wspace happy.

--- bodil/bodil-codestyle.el
+++ bodil/bodil-codestyle.el
@@ -31,6 +31,8 @@
 
 ;; Enforce proper whitespace
 (package-require 'ethan-wspace)
+(setq mode-require-final-newline nil)
+(setq require-final-newline nil)
 (global-ethan-wspace-mode 1)
 
 ;; Clojure indentation rules
