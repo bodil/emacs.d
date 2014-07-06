5edc871277192e89d134b5b5892217bc3e366d4b
Bodil Stokke <bodil@bodil.org>
2014-07-06 20:56:01 +0100

    More mu4e tweaks.

--- bodil/bodil-mail.el
+++ bodil/bodil-mail.el
@@ -7,23 +7,28 @@
 
 ;; Maildir setup
 (setq
- mu4e-maildir "~/.mail/fastmail"
- mu4e-sent-folder "/Sent Items"
+ mu4e-maildir "~/.mail"
+ mu4e-sent-folder "/Sent"
  mu4e-drafts-folder "/Drafts"
  mu4e-trash-folder "/Trash"
  mu4e-refile-folder "/Archive"
- mu4e-get-mail-command "mbsync fastmail-sync"
+ mu4e-get-mail-command "offlineimap"
  mu4e-update-interval nil
  mu4e-sent-messages-behavior 'delete
  mu4e-headers-include-related t
- mu4e-headers-skip-duplicates t)
+ mu4e-headers-skip-duplicates t
+ mu4e-confirm-quit nil
+ mu4e-maildir-shortcuts
+ '(("/INBOX" . ?i)
+   ("/Future Ad Labs" . ?f)
+   ("/Lambda Ladies" . ?l)
+   ("/LdnClj" . ?c)))
 
 ;; User info
 (setq
  user-mail-address "bodil@bodil.org"
  user-full-name "Bodil Stokke"
- message-signature "Bodil Stokke <bodil@bodil.org>"
- message-signature-file "~/.signature"
+ mu4e-compose-signature "Bodil Stokke <bodil@bodil.org>\n"
  mu4e-user-mail-address-list
  '("bodil@bodil.org"
    "bodil@fastmail.co.uk"
@@ -34,14 +39,18 @@
  mu4e-attachment-dir "~/Downloads")
 
 ;; SMTP
+(package-require 'async)
+(require 'smtpmail-async)
 (setq
- message-send-mail-function 'smtpmail-send-it
- smtpmail-stream-type 'ssl
- smtpmail-default-smtp-server "mail.messagingengine.com"
- smtpmail-smtp-server "mail.messagingengine.com"
- smtpmail-smtp-service 465
+ send-mail-function 'async-smtpmail-send-it
+ message-send-mail-function 'async-smtpmail-send-it
+ smtpmail-stream-type 'starttls
+ smtpmail-default-smtp-server "mail.lol.camp"
+ smtpmail-smtp-user "bodil@bodil.org"
+ smtpmail-smtp-server "mail.lol.camp"
+ smtpmail-smtp-service 587
  smtpmail-queue-mail nil
- smtpmail-queue-dir "~/.mail/queue/cur")
+ smtpmail-queue-dir "~/.mail/Queue/cur")
 
 ;; Headers view
 (setq
@@ -58,8 +67,10 @@
 
 ;; Message view
 (setq
+ mu4e-html2text-command "w3m -dump -T text/html -cols 65536"
  mu4e-view-prefer-html t
  mu4e-view-show-images t)
+
 (add-hook 'mu4e-view-mode-hook (lambda () (visual-line-mode)))
 (when (fboundp 'imagemagick-register-types)
   (imagemagick-register-types))
@@ -68,30 +79,30 @@
  'mu4e-view-actions
  '("ViewInBrowser" . mu4e-action-view-in-browser) t)
 
-;; 1) messages to me@foo.com should be replied with From:me@foo.com
-;; 2) messages to me@bar.com should be replied with From:me@bar.com
-;; 3) all other mail should use From:me@cuux.com
-;; (add-hook
-;;  'mu4e-compose-pre-hook
-;;  (defun my-set-from-address ()
-;;    "Set the From address based on the To address of the original."
-;;    (let ((msg mu4e-compose-parent-message)) ;; msg is shorter...
-;;      (setq user-mail-address
-;;            (cond
-;;             ((mu4e-message-contact-field-matches msg :to "me@foo.com")
-;;              "me@foo.com")
-;;             ((mu4e-message-contact-field-matches msg :to "me@bar.com")
-;;              "me@bar.com")
-;;             (t "me@cuux.com"))))))
-
 ;; Compose view
 (define-key mu4e-compose-mode-map (kbd "C-c s") 'mml-secure-message-sign-pgpmime)
 (define-key mu4e-compose-mode-map (kbd "C-c e") 'mml-secure-message-encrypt-pgpmime)
 
 (add-hook
+ 'mu4e-compose-pre-hook
+ (lambda ()
+   "Set the From address based on the To address of the original."
+   (let ((msg mu4e-compose-parent-message))
+     (when msg
+       (setq user-mail-address
+             (cond
+              ((mu4e-message-contact-field-matches msg :to "@futureadlabs.com$")
+               "bodil@futureadlabs.com")
+              ((mu4e-message-contact-field-matches msg :to "@doge.enterprises$")
+               "bodil@doge.enterprises")
+              (t "bodil@bodil.org")))))))
+
+
+(add-hook
  'mu4e-compose-mode-hook
  (lambda ()
-   (set-fill-column 72)
+   (visual-line-mode)
+   (auto-fill-mode 0)
    (flyspell-mode)))
 
 (provide 'bodil-mail)
