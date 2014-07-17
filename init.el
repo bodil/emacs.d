6e44900aa530592c221773b3e1c4bae4d8e2fe59
Bodil Stokke <bodil@bodil.org>
2014-07-17 01:21:05 +0100

    Disable Marmalade.

--- init.el
+++ init.el
@@ -62,7 +62,7 @@
 (setq package-user-dir (concat dotfiles-dir "elpa"))
 (require 'package)
 (dolist (source '(("melpa" . "http://melpa.milkbox.net/packages/")
-                  ("marmalade" . "http://marmalade-repo.org/packages/")
+                  ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                   ("elpa" . "http://tromey.com/elpa/")))
   (add-to-list 'package-archives source t))
 (package-initialize)
