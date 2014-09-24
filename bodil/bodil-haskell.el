c27595d6478b9465a1637f3d6b48fe1ec686f144
Bodil Stokke <bodil@bodil.org>
2014-09-24 22:12:11 +0100

    Purscheck.

--- bodil/bodil-haskell.el
+++ bodil/bodil-haskell.el
@@ -74,5 +74,25 @@
 (add-to-list 'auto-mode-alist (cons "\\.purs\\'" 'purescript-mode))
 (setup-haskell-arrows 'purescript-mode purescript-mode-map)
 
+(eval-after-load 'flycheck
+  '(progn
+     (flycheck-define-checker purs-check
+       "Use purscheck to flycheck PureScript code."
+       :command ("purscheck" source source-original temporary-file-name)
+       :error-patterns
+       ((error line-start
+               (or (and "Error at " (file-name)    " line " line ", column " column ":"
+                        (zero-or-more " "))
+                   (and "\""        (file-name) "\" (line " line ", column " column "):"))
+               (or (message (one-or-more not-newline))
+                   (and "\n"
+                        (message
+                         (zero-or-more " ") (one-or-more not-newline)
+                         (zero-or-more "\n"
+                                       (zero-or-more " ")
+                                       (one-or-more not-newline)))))
+               line-end))
+       :modes purescript-mode)
+     (add-to-list 'flycheck-checkers 'purs-check)))
 
 (provide 'bodil-haskell)
