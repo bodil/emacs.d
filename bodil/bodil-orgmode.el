751c78ac0cbd335935af075dd7c25c8f5efb1306
Bodil Stokke <bodil@bodil.org>
2014-10-31 15:27:47 +0000

    Always use visual-line-mode in org-mode.

--- bodil/bodil-orgmode.el
+++ bodil/bodil-orgmode.el
@@ -3,4 +3,10 @@
 ;; Stop org-mode from highjacking shift-cursor keys
 (setq org-replace-disputed-keys t)
 
+;; Always use visual-line-mode in org-mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            (visual-line-mode 1)
+            (set-visual-wrap-column 80)))
+
 (provide 'bodil-orgmode)
