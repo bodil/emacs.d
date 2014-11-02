d3c7de7dda1dc55d364152b6041db5a86a25dd53
Bodil Stokke <bodil@bodil.org>
2014-11-02 20:20:46 +0000

    Considerably improved Dilbert loader, with more puppies. cc @mickeynp

--- bodil/bodil-ohai.el
+++ bodil/bodil-ohai.el
@@ -3,30 +3,39 @@
 
 (package-require 'dash)
 (package-require 's)
-(package-require 'f)
 (package-require 'web)
 
 (require 'dash)
 (require 's)
-(require 'f)
 (require 'web)
 
-(defun ohai--dilbert-url (cb)
+(defun ohai--load-and-map (url map-fn cb)
   (web-http-get
    (lambda (con header data)
-     (-if-let (url (s-match "/dyn/str_strip/[0-9/]+.strip.gif" data))
-         (funcall cb (s-concat "http://dilbert.com" (car url)))
-       (funcall cb "http://placepuppy.it/600/400")))
-   :url "http://dilbert.com/"))
+     (funcall cb (funcall map-fn data header)))
+   :url url))
 
-(defun ohai--load-url (url cb)
+(defun ohai--load-and-match (url regex format-str cb)
+  (ohai--load-and-map
+   url
+   (lambda (data header)
+     (-when-let (m (s-match regex data))
+         (s-format format-str 'elt m)))
+   cb))
+
+(defun ohai--pick-first (fetchers cb)
+  (-if-let (fetcher (car fetchers))
+      (if (stringp fetcher) (funcall cb fetcher)
+        (funcall fetcher
+                 (lambda (url)
+                   (if url (funcall cb url)
+                     (ohai--pick-first (cdr fetchers) cb)))))
+    (funcall cb nil)))
+
+(defun ohai--load-image (url cb)
   (web-http-get
    (lambda (con header data)
-     (let ((filename (make-temp-file
-                      "loadurl" nil
-                      (s-concat "." (ohai--guess-extension header url)))))
-       (f-write-bytes (string-to-unibyte data) filename)
-       (funcall cb filename)))
+     (funcall cb (create-image (string-to-unibyte data) nil t)))
    :url url))
 
 (defun ohai--display-image (img)
@@ -34,24 +43,50 @@
     (insert-image img "ohai")
     (insert "\n\n")))
 
-(defun ohai--guess-extension (header url)
-  (let ((type (gethash 'content-type header))
-        (ext (f-ext url)))
-    (if header
-        (cond
-         ((string= type "image/gif") "gif")
-         ((string= type "image/jpeg") "jpg")
-         ((string= type "image/png") "png")
-         ((string= type "image/svg") "svg"))
-      ext)))
+(defun ohai-dilbert (cb)
+  (ohai--load-and-match "http://dilbert.com/"
+                        "/dyn/str_strip/[0-9/]+\.strip\\(\.sunday\\)?\.gif"
+                        "http://dilbert.com$0" cb))
 
-(defun ohai-dilbert ()
-  (ohai--dilbert-url
+(defun ohai-imgur (search-term)
+  (lambda (cb)
+    (ohai--load-and-match
+     (s-concat "http://imgur.com/search/score/day?q_size_px=med&q_size_mpx=med&q="
+               (url-hexify-string search-term))
+     "<div id=\"\\([0-9a-zA-Z]+\\)\" class=\"post\""
+     "http://i.imgur.com/$1.jpg" cb)))
+
+(defun ohai-placepuppy (cb)
+  (cb "http://placepuppy.it/600/400"))
+
+(setq-default
+ ohai-sources
+ (list 'ohai-dilbert
+       (ohai-imgur "puppy")
+       'ohai-placepuppy))
+
+(defun ohai (&optional sources &optional cb)
+  (interactive)
+  (ohai--pick-first
+   (or sources ohai-sources)
    (lambda (url)
-     (ohai--load-url url (lambda (imgpath) (ohai--display-image (create-image imgpath)))))))
+     (ohai--load-image
+      url (lambda (img)
+            (ohai--display-image img)
+            (when cb (funcall cb)))))))
 
-;; (setq max-image-size nil)
+(defun ohai-go-dilbert-and-puppies ()
+  (ohai (list 'ohai-dilbert)
+        (lambda ()
+          (ohai (list (ohai-imgur "puppy")
+                      'ohai-placepuppy)
+                (lambda ()
+                  (with-current-buffer (get-buffer "*scratch*")
+                    (beginning-of-buffer)
+                    (line-move 3))))))
+  t)
+
 (when (not on-console)
-  (setq initial-buffer-choice (lambda () (ohai-dilbert) t)))
+  (setq initial-buffer-choice 'ohai-go-dilbert-and-puppies))
 
 (provide 'bodil-ohai)
