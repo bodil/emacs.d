9c3595f9b1e965072f16154f52ea464b02c9bb1c
Bodil Stokke <bodil@bodil.org>
2015-01-15 16:52:24 +0000

    Flycheck tweaks.

--- bodil/bodil-flycheck.el
+++ bodil/bodil-flycheck.el
@@ -9,16 +9,32 @@
 (global-set-key (kbd "M-n") 'next-error)
 (global-set-key (kbd "M-p") 'previous-error)
 
+(global-set-key
+ (kbd "C-c c")
+ (lambda () (interactive)
+   (if flycheck-checker
+       (progn
+         (save-buffer)
+         (flycheck-compile flycheck-checker)))
+   (message
+    "No checker selected for this buffer. Try M-x flycheck-select-checker")))
+
 (package-require 'flycheck-color-mode-line)
 
 (eval-after-load "flycheck"
   '(progn
-     (setq flycheck-highlighting-mode nil)
+     (setq flycheck-highlighting-mode 'symbols)
      (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
-     (set-face-background 'flycheck-error "red")
-     (set-face-foreground 'flycheck-error "black")
-     (set-face-background 'flycheck-warning "orange")
-     (set-face-foreground 'flycheck-warning "black")))
+     (set-face-background 'flycheck-error "#660000")
+     (set-face-foreground 'flycheck-error nil)
+     (set-face-background 'flycheck-warning "#775500")
+     (set-face-foreground 'flycheck-warning nil)
+     (set-face-background 'flycheck-color-mode-line-error-face "#660000")
+     (set-face-background 'flycheck-color-mode-line-warning-face "#553300")
+     (set-face-background 'flycheck-color-mode-line-info-face nil)
+     (set-face-foreground 'flycheck-color-mode-line-error-face nil)
+     (set-face-foreground 'flycheck-color-mode-line-warning-face nil)
+     (set-face-foreground 'flycheck-color-mode-line-info-face nil)))
 
 ;;; Haskell specific
 
